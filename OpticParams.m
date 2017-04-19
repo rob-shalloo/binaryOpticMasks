774df7cd4db3edd31f412ece6496e374912d22e7
Rob Shalloo <Rob@temps-mbp.nat.physics.ox.ac.uk>
2017-04-19 17:03:48 +0100

    Added basic inputs for the optic

--- OpticParams.m
+++ OpticParams.m
@@ -1,1 +1,14 @@
-%OpticParams
+%% Optic Parameters
+
+% Physical parameters of optic
+diam = 50.8e-3;     % diameter of optic (mm)
+
+
+% Beam Parameters
+lambda = 0.8        % wavelength of Ti:Sapphire (um)
+
+% Photolithography Params
+numLayers = 2       % How many layers are to be used in the optic
+resolution = 3.0    % minimum feature size which can be etched (um)
+
+
